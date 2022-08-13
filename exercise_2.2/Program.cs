ab4529beb1f17c319476e111cec8c2a743f23e50
Alexander_Napreenko <sasha-napreenko@yandex.ru>
2022-08-13 16:22:25 +0300

    add exercise_2.2

---
+++ exercise_2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number, number2;
+
+Console.Write("Введите любое число: ");
+
+number = Convert.ToInt32(Console.ReadLine());
+
+string number3 = Convert.ToString(number);
+
+number2 = number3.Length;
+
+if (number2 > 2)
+{
+    Console.WriteLine("Третья цифра заданного числа: " + number3[2]);
+}
+else Console.WriteLine("третьей цифры нет");
